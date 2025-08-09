60c52ae5a8803917e605596dc274e87b51ddfe6e
aracope <156398622+aracope@users.noreply.github.com>
2025-08-09 13:01:55 -0600

    Set up .env, adjust config.js to actually launch app

--- backend/config.js
+++ backend/config.js
@@ -5,10 +5,6 @@
 require("dotenv").config();
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-
-const PORT = +process.env.PORT || 3001;
-
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
@@ -22,15 +18,14 @@ function getDatabaseUri() {
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("Jobly Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
 console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
 
 module.exports = {
-  SECRET_KEY,
-  PORT,
+  SECRET_KEY: process.env.SECRET_KEY || "secret-dev",
+  PORT: +process.env.PORT || 3001,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
 };
+
